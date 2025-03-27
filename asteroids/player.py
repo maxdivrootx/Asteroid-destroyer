9931a3da4fc22ba99323681052c8554cc1ac19d4
maxdivrootx <stansmithme7003@gmail.com>
2025-03-27 07:32:55 +0000

    added player file

---
+++ asteroids/player.py
@@ -0,0 +1,77 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+# At the top of your player.py file, after imports:
+print("Constants imported:", dir())
+print("SHOT_RADIUS:", SHOT_RADIUS)  # This should show the value
+
+class Shot(CircleShape):
+    
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.velocity = pygame.Vector2(0, 0)
+    
+    def update(self, dt):
+        # Update position based on velocity
+        self.position += self.velocity * dt
+    
+    def draw(self, screen):
+        # Draw the shot as a circle
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius)
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.timer = 0 
+
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle())
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.timer -= dt
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
+    # Create a new shot at the player's position
+        shot = Shot(self.position.x, self.position.y)
+    
+    # Set shot velocity in the direction the player is facing
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
+
+
+
